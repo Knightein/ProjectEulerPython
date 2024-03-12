3c61749b79ed54cc0a36a464b35e4bbe872bc84e
Knightein <oliver.mollitt123@gmail.com>
2024-03-12 00:32:13 +0000

    Problem 1 Solved

---
+++ Problem1/Problem1.py
@@ -0,0 +1,7 @@
+def problem1_solution(number):
+    sum = 0
+    for i in range(number):
+        # Check modular of 3 or 5, and add to sum
+        if i % 3 == 0 or i % 5 == 0:
+            sum += i
+    return sum
